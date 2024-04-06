674561a0638ed079be51a6e85705eb3fab826cea
jagger-zlotoff <jzlotoff@uccs.edu>
2024-04-06 17:54:50 -0600

    up and running

--- rockclimb/django_project/settings.py
+++ rockclimb/django_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rockclimb_app',
+]
+
+# Add support for authenticating users
+AUTHENTICATION_BACKENDS = [
+'django.contrib.auth.backends.ModelBackend',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 
 ROOT_URLCONF = 'django_project.urls'
 
@@ -121,3 +128,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+
+STATIC_URL = 'static/'
+
+
+STATICFILES_DIRS = [
+os.path.join(BASE_DIR, 'static')
+]
+
+
+MEDIA_URL = '/images/'
---
+++ rockclimb/rockclimb_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RockclimbAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'rockclimb_app'
