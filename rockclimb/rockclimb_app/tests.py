fdca0d72c3d03acef0a885691cd40f85295f576c
jagger-zlotoff <jzlotoff@uccs.edu>
2024-04-19 13:17:18 -0600

    got selenium downloaded and the login page is being tested.

--- rockclimb/rockclimb_app/tests.py
+++ rockclimb/rockclimb_app/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
 
 # Create your tests here.
+class PlayerFormTest(LiveServerTestCase):
+
+  def testform(self):
+    selenium = webdriver.Chrome()
+    #Choose your url to visit
+    selenium.get('http://localhost:8000/login/?next=/')
+    #find the elements you need to submit form
+    user = selenium.find_element(By.ID, 'id_username')
+    password = selenium.find_element(By.ID, 'id_password')
+
+
+    submit = selenium.find_element(By.ID, 'submit')
+
+    #populate the form with data
+    user.send_keys('test_user')
+    password.send_keys('Pass2rock')
+
+    #submit form
+    submit.send_keys(Keys.RETURN)
+
+    #check result; page source looks at entire html document
+    assert 'test_user' in selenium.page_source
